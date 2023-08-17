ca6be69a6f2fd1d7cdb3ed5da221cb35f9edbaea
Francis Babalola <fobabs2win@rocketmail.com>
2023-08-17 23:42:11 +0100

    Richest Customer Wealth

---
+++ src/Richest_Customer_Wealth/solution.js
@@ -0,0 +1,38 @@
+/**
+ * Returns the wealth that the richest customer has
+ * @param {number[]} accounts
+ * @returns {num} - Returns a number
+ */
+// Recursive Method
+const maximumWealth = (accounts) => {
+  let richestCustomer = 0;
+  for (let i = 0; i < accounts.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < accounts[i].length; j++) {
+      sum += accounts[i][j];
+    }
+    richestCustomer = Math.max(richestCustomer, sum);
+  }
+  return richestCustomer;
+};
+
+// console.log(
+//   maximumWealth([
+//     [1, 2, 3],
+//     [3, 2, 1],
+//   ])
+// );
+// console.log(
+//   maximumWealth([
+//     [1, 5],
+//     [7, 3],
+//     [3, 5],
+//   ])
+// );
+// console.log(
+//   maximumWealth([
+//     [2, 8, 7],
+//     [7, 1, 3],
+//     [1, 9, 5],
+//   ])
+// );
