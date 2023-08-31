74add0df19a09a4a1559aff9f6c891a39eeec124
Francis Babalola <fobabs2win@rocketmail.com>
2023-08-31 11:28:16 +0100

    Duplicate Zeros

---
+++ src/Duplicate_Zeros/solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+
+// Imperative method
+const duplicateZeros = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      for (let j = arr.length - 1; j > i; j--) {
+        arr[j] = arr[j - 1];
+      }
+      i++;
+    }
+  }
+};
+
+// console.log(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0]));
+// console.log(duplicateZeros([1, 2, 3]));
