493a47c3f6aeb72f09ce8371379c56525c92b3f9
Francis Babalola <fobabs2win@rocketmail.com>
2023-09-11 19:52:42 +0100

    feat(ts): Number of Steps to Reduce a Number to Zero

---
+++ src/Number_of_Steps_to_Reduce_a_Number_to_Zero/solution.ts
@@ -0,0 +1,38 @@
+/**
+ * Returns the number of steps to reduce it to zero
+ * @param {number} num
+ * @return {number}
+ */
+
+// Using imperative method
+// while loop
+// const numberOfSteps = (num: number): number => {
+//   let count = 0
+//   while (num > 0) {
+//     if (num % 2 == 0) {
+//       num /= 2
+//     } else {
+//       num--
+//     }
+//     count++
+//   }
+//   return count
+// }
+
+//for loop (Higher speed)
+const numberOfSteps = (num: number): number => {
+  let count = 0
+  for (let i = num; i > 0;) {
+    if (i % 2 == 0) {
+      i /= 2
+    } else {
+      i--
+    }
+    count++
+  }
+  return count
+}
+
+// console.log(numberOfSteps(14));
+// console.log(numberOfSteps(8));
+// console.log(numberOfSteps(123));
