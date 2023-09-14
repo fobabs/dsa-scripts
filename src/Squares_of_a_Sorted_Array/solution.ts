ca06a32913d64a68140640ad18bb39fc75b59761
Francis Babalola <fobabs2win@rocketmail.com>
2023-09-14 20:55:46 +0100

    feat(ts): Squares of a Sorted Array

---
+++ src/Squares_of_a_Sorted_Array/solution.ts
@@ -0,0 +1,31 @@
+/**
+ * Returns an array of the squares of each number sorted in non-decreasing order
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+// Declarative method - Functional programming (using map and sort)
+// const sortedSquares = (nums: number[]): number[] => {
+//   let newArray = nums.map(num => num ** 2)
+//   return newArray.sort((a, b) => a - b)
+// }
+
+// Imperative method (for loop) - Faster method
+const sortedSquares = (nums: number[]): number[] => {
+  let resultArr: number[] = []
+  for (let i = 0; i < nums.length; i++) {
+    // resultArr[i] = Math.pow(nums[i], 2)
+    const squaredElement = nums[i] * nums[i]
+    // Locate the right position to insert the squared element into the resultArr array
+    let index = 0
+    while (index < resultArr.length && squaredElement > resultArr[index]) {
+      index++
+    }
+    // Insert the squared element into the resultArr array at the right position
+    resultArr.splice(index, 0, squaredElement)
+  }
+  return resultArr
+}
+
+// console.log(sortedSquares([-4, -1, 0, 3, 10]));
+// console.log(sortedSquares([-7, -3, 2, 3, 11]));
