44645cc85765dc061463cf21dc33e6e09ffb4446
Francis Babalola <fobabs2win@rocketmail.com>
2023-09-19 07:54:31 +0100

    refactor(js): add descriptive comment (Richest Customer Wealth)

--- src/Richest_Customer_Wealth/solution.js
+++ src/Richest_Customer_Wealth/solution.js
@@ -1,29 +1,29 @@
 /**
  * Returns the wealth that the richest customer has
  * @param {number[][]} accounts
- * @returns {number} - Returns a number
+ * @return {number} - Returns a number
  */
-// Recursive Method
-const maximumWealth = (accounts) => {
-  let richestCustomer = 0;
-  for (let i = 0; i < accounts.length; i++) {
-    let sum = 0;
-    for (let j = 0; j < accounts[i].length; j++) {
-      sum += accounts[i][j];
-    }
-    richestCustomer = Math.max(richestCustomer, sum);
-  }
-  return richestCustomer;
-};
-
-// Functional Programming method using map and reduce
+// Imperative method using for loop (Recursion)
 // const maximumWealth = (accounts) => {
-//   return Math.max(
-//     ...accounts.map((account) => {
-//       return account.reduce((prev, curr) => prev + curr, 0);
-//     })
-//   );
+//   let richestCustomer = 0;
+//   for (let i = 0; i < accounts.length; i++) {
+//     let sum = 0;
+//     for (let j = 0; j < accounts[i].length; j++) {
+//       sum += accounts[i][j];
+//     }
+//     richestCustomer = Math.max(richestCustomer, sum);
+//   }
+//   return richestCustomer;
 // };
+
+// Declarative method - Functional Programming method using map and reduce
+const maximumWealth = (accounts) => {
+  return Math.max(
+    ...accounts.map((account) => {
+      return account.reduce((prev, curr) => prev + curr, 0);
+    })
+  );
+};
 
 // console.log(
 //   maximumWealth([
