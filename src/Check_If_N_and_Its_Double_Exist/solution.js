62c7d07288b9f757a26cefdb2e0f56bae9db38cc
Francis Babalola <fobabs2win@rocketmail.com>
2023-09-29 14:57:04 +0100

    feat(js): Check If N and Its Double Exist

---
+++ src/Check_If_N_and_Its_Double_Exist/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+
+const checkIfExist = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === 2 * arr[j] || (arr[i] === 2 / arr[j] && i != j)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+// console.log(checkIfExist([10, 2, 5, 3]));
+// console.log(checkIfExist([3, 1, 7, 11]));
