10ca58d8e938a79e9f00555b6d257a7a6dbc1714
Francis Babalola <fobabs2win@rocketmail.com>
2023-10-16 09:37:59 +0100

    feat(js): Third Maximum Number

---
+++ src/Third_Maximum_Number/solution.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// Using .sort() and for loop method
+// const thirdMax = (nums) => {
+//   nums.sort((a, b) => b - a);
+
+//   let count = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (!nums[i + 1] || nums[i] !== nums[i + 1]) {
+//       count++;
+//       if (count === 3) return nums[i];
+//     }
+//   }
+//   return nums[0];
+// };
+
+// Using for loop method
+const thirdMax = (nums) => {
+  let first = -Infinity;
+  let second = -Infinity;
+  let third = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === first || nums[i] === second || nums[i] === third) continue;
+    if (nums[i] > first) {
+      [first, second, third] = [nums[i], first, second];
+    } else if (nums[i] > second) {
+      [second, third] = [nums[i], third];
+    } else if (nums[i] > third) {
+      third = nums[i];
+    }
+  }
+  return third === -Infinity ? first : third;
+};
+
+// console.log(thirdMax([3, 2, 1]));
+// console.log(thirdMax([1, 2]));
+// console.log(thirdMax([2, 2, 3, 1]));
