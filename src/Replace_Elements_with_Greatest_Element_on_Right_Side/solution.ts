35bf2ca49449f99c7d61eb9f1dad9f7f883de6b2
Francis Babalola <fobabs2win@rocketmail.com>
2023-11-01 09:55:00 +0100

    feat(ts): Replace Elements with Greatest Element on Right Side

---
+++ src/Replace_Elements_with_Greatest_Element_on_Right_Side/solution.ts
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+
+const replaceElements = (arr: number[]): number[] => {
+  let max = -1
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let curr = arr[i]
+    arr[i] = max
+    max = Math.max(curr, max)
+    // if (curr > max) {
+    //   max = curr
+    // }
+  }
+  return arr
+}
+
+// console.log(replaceElements([17, 18, 5, 4, 6, 1]))
+// console.log(replaceElements([400]))
