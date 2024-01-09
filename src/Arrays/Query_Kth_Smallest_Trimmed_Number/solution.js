634f343134b38a171353e13a9d87dd8450cfecd2
FOBABS <fobabs2win@rocketmail.com>
2024-01-09 10:55:19 +0100

    feat(js): Add for of method (Query Kth Smallest Trimmed Number)

--- src/Arrays/Query_Kth_Smallest_Trimmed_Number/solution.js
+++ src/Arrays/Query_Kth_Smallest_Trimmed_Number/solution.js
@@ -7,21 +7,37 @@
 const smallestTrimmedNumbers = (nums, queries) => {
     const answer = [];
 
-    for (let i = 0; i < queries.length; i++) {
-      const [k, trim] = queries[i];
-      const trimmedNums = nums.map(num => parseInt(num.slice(-trim))); // Trim each number to 'trim' rightmost digits
-      const indices = Array.from({ length: nums.length }, (_, i) => i); // Create an array of indices
+    // for (let i = 0; i < queries.length; i++) {
+    //   const [k, trim] = queries[i];
+    //   const trimmedNums = nums.map(num => parseInt(num.slice(-trim))); // Trim each number to 'trim' rightmost digits
+    //   const indices = Array.from({ length: nums.length }, (_, i) => i); // Create an array of indices
       
-      // Sort indices based on the trimmed numbers and original indices
+    //   // Sort indices based on the trimmed numbers and original indices
+    //   indices.sort((a, b) => {
+    //     if (trimmedNums[a] !== trimmedNums[b]) {
+    //       return trimmedNums[a] - trimmedNums[b];
+    //     } else {
+    //       return a - b; // If the trimmed numbers are equal, compare original indices
+    //     }
+    //   });
+
+    //   answer.push(indices[k - 1]); // Push the k-th smallest index after sorting
+    // }
+
+    for (let [k, trim] of queries) {
+      const trimmedNums = nums.map(num => parseInt(num.slice(-trim))) // Trim each number to 'trim' rightmost digits
+      const indices = Array.from({ length: nums.length }, (_, i) => i) // Create an array of indices
+
+      // sort indices based on the trimmed numbers nd original indices
       indices.sort((a, b) => {
         if (trimmedNums[a] !== trimmedNums[b]) {
-          return trimmedNums[a] - trimmedNums[b];
+          return trimmedNums[a] - trimmedNums[b]
         } else {
-          return a - b; // If the trimmed numbers are equal, compare original indices
+          return a - b // if the trimmed numbers are equal, compare original indices
         }
-      });
-
-      answer.push(indices[k - 1]); // Push the k-th smallest index after sorting
+      })
+      
+      answer.push(indices[k - 1]) // Push the k-th smallest index after sorting
     }
 
     return answer;
