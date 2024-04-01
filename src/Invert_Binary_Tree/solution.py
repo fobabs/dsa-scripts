0a00a4fab5bbe367fbf92ccdc515f2dc2a256da8
FOBABS <fobabs2win@rocketmail.com>
2024-04-01 23:23:34 +0100

    feat(py): Invert Binary Tree

---
+++ src/Invert_Binary_Tree/solution.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val: int = 0, left: "TreeNode" = None, right: "TreeNode" = None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+class Solution:
+    def invertTree(self, root: "TreeNode") -> "TreeNode": 
+        if root is None:
+            return None
+        
+        # Swap the left and right children of the current node
+        root.left, root.right = root.right, root.left
+        
+        # Recursively invert the left and right subtrees
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+    
